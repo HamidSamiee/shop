4ad7fd6731de1f46ab3625224dd06faaf5fd29ea
hamidsmoaser <hamidsmoaser@yahoo.com>
2024-05-15 14:20:53 +0330

    set interceptors for refresh-token  to use login user

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`${vazirFont.variable} font-sans`}>
+      <body className={`${vazirFont.variable} font-sans`} suppressHydrationWarning={true}>
         <Providers>
           <Toaster />
           <Header />
